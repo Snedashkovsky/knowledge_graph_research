fe7c9db90756f8653f526d60fee1c1aa863918e9
Snedashkovsky <snedashkovsky@gmail.com>
2021-05-27 18:50:02 +0800

    Add `.env` file: - add .env.example - update `config.py` - update `.gitignore` - update `requirements.txt` Add utils for cyberLink creation: - add `utils_bash.py` - add `utils_ipfs.py` - add `create_cyberlink.sh`

--- config.py
+++ config.py
@@ -1,6 +1,13 @@
 from clickhouse_driver import Client
+from dotenv import dotenv_values
 
-CLICKHOUSE_HOST = 'localhost'
-COMPRESSION = 'lz4'
-client = Client(host=CLICKHOUSE_HOST)
+CLICKHOUSE_HOST = dotenv_values(".env")['CLICKHOUSE_HOST']
+CLICKHOUSE_COMPRESSION = dotenv_values(".env")['CLICKHOUSE_COMPRESSION']
+client = Client(host=CLICKHOUSE_HOST, compression=CLICKHOUSE_COMPRESSION)
 execute = client.execute
+
+IPFS_HOST = dotenv_values(".env")['IPFS_HOST']
+
+CYBERLINK_CREATION_QUERY = dotenv_values(".env")['CYBERLINK_CREATION_QUERY']
+
+BASH_TIMEOUT = 20
---
+++ src/utils_bash.py
@@ -0,0 +1,43 @@
+from subprocess import Popen, PIPE
+import logging
+
+from config import CYBERLINK_CREATION_QUERY, BASH_TIMEOUT
+
+
+def execute_bash(bash_command):
+    process = Popen(bash_command.split(), stdout=PIPE)
+    return process.communicate(timeout=BASH_TIMEOUT)
+
+
+def extract_from_console(console_output, keys):
+    console_output = [item.replace(' ', '').split(':') for item in str(console_output).split('\\n')]
+    return [[item[0], item[1].split('\\')[0]] for item in console_output if item[0] in keys]
+
+
+def create_cyberlink(account_name, from_hash, to_hash, query=CYBERLINK_CREATION_QUERY):
+    try:
+        output, error_execute_bash = execute_bash(f'{query} {account_name} {from_hash} {to_hash}')
+        if error_execute_bash:
+            logging.error(
+                f"cyberLink was not created. Account {account_name}, from {from_hash}, to {to_hash}. "
+                f"Error {error_execute_bash}")
+            return None, error_execute_bash
+        rawlog = extract_from_console(output, ['rawlog'])[0][1]
+        if rawlog == 'not enough personal bandwidth'.replace(' ', ''):
+            logging.info(
+                f"cyberLink was not created. Account {account_name}, from {from_hash}, to {to_hash}. Not enough "
+                f"personal bandwidth")
+            return None, 'not enough personal bandwidth'
+        tx_hash = extract_from_console(output, ['txhash'])[0][1]
+        logging.info(
+            f"cyberLink was created. Account {account_name}, from {from_hash}, to {to_hash}. tx {tx_hash}")
+        return tx_hash, None
+    except Exception as error_parsing:
+        logging.error(
+            f"cyberLink was not created. Account {account_name}, from {from_hash}, to {to_hash}. Error {error_parsing}")
+        return None, error_parsing
+
+
+def test_create_cyberlink():
+    tx_hash, _ = create_cyberlink('', '', '', query='cat ./tests/create_cyberlink_test')
+    assert tx_hash == 'C97489299E9FE23FDE5F85AF85C076D869648577A9EE914E5A0332A6C515DE46'
